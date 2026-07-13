b6051127956e1814253b0585357241c465234350
jibola210 <agboladeridwan174@gmail.com>
2026-07-13 03:52:12 -0700

    Anniversary website

---
+++ js/script.js
@@ -0,0 +1,47 @@
+window.addEventListener("load", () => {
+
+    // Hide loader after 3 seconds
+    const loader = document.querySelector(".loader");
+
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 3000);
+
+    // Anniversary Counter
+    const startDate = new Date("August 11, 2025 00:00:00");
+
+    function updateCounter(){
+
+        const now = new Date();
+
+        const difference = now - startDate;
+
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+        const hours = Math.floor(
+            (difference % (1000 * 60 * 60 * 24))
+            / (1000 * 60 * 60)
+        );
+
+        const minutes = Math.floor(
+            (difference % (1000 * 60 * 60))
+            / (1000 * 60)
+        );
+
+        const seconds = Math.floor(
+            (difference % (1000 * 60))
+            / 1000
+        );
+
+        document.getElementById("days").textContent = days;
+        document.getElementById("hours").textContent = hours;
+        document.getElementById("minutes").textContent = minutes;
+        document.getElementById("seconds").textContent = seconds;
+
+    }
+
+    updateCounter();
+
+    setInterval(updateCounter,1000);
+
+});
